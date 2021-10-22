0f3431bc9b59d2e999d07f5b878c9bef8727d537
kalyan <kalyan@LAPTOP-UMFJ6OLA>
2021-10-22 16:44:21 +0530

    commit

--- src/main/java/com/meetmyservice/generics/LoginSushiTest.java
+++ src/main/java/com/meetmyservice/generics/LoginSushiTest.java
@@ -5,5 +5,6 @@ public class LoginSushiTest {
 	// TODO Auto-generated method stub
 System.out.println("hellosushi");
 System.out.println("hi" );
+System.out.println("hi this is sushi have a nice day");
 }
 }

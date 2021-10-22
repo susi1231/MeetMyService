9b039828fc7a5594cbc33abc126a2a7c04fe26b6
Admin <Admin@192.168.2.3>
2021-10-22 17:02:44 +0530

    test new

--- src/main/java/com/meetmyservice/generics/LoginSushiTest.java
+++ src/main/java/com/meetmyservice/generics/LoginSushiTest.java
@@ -8,5 +8,6 @@ System.out.println("hi" );
 System.out.println("hi this is shruthi" );
 System.out.println("hi this is shruthi cr " );
 System.out.println("hi this is omkar");
+System.out.println("check new omkar");
 }
 }

7a96aeff779ff9cda108600abf8ceb5b7e399b84
susi1231 <kanisirisusi@gmail.com>
2021-10-22 19:34:08 +0530

    change

--- src/main/java/com/meetmyservice/generics/LoginSushiTest.java
+++ src/main/java/com/meetmyservice/generics/LoginSushiTest.java
@@ -9,5 +9,7 @@ System.out.println("hi this is shruthi" );
 System.out.println("hi this is shruthi cr " );
 System.out.println("hi this is omkar");
 System.out.println("check new omkar");
+System.out.println("my new test");
+System.out.println("check this dummy data");
 }
 }
